72180eeb4d5c430264e3d1559b19c91fa1464666
duncanlaidlaw1 <slamdunc123@gmail.com>
2020-05-06 20:54:29 +0100

    refactor Recipes component to use hooks

--- src/components/pages/Recipes/Recipes.js
+++ src/components/pages/Recipes/Recipes.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import domain from '../../../domain';
 import Recipe from './Recipe';
@@ -7,101 +7,67 @@ import LoadingSpinner from '../../partials/LoadingSpinner';
 import Paginate from '../../partials/Paginate';
 import './recipes.scss';
 
-export default class Recipes extends Component {
-	state = {
-		recipes: [],
-		isLoading: true,
-		activePage: 1,
-		itemsPerPage: 5,
-		pageRange: 5,
-	};
+const Recipes = () => {
+	const [recipes, setRecipes] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [activePage, setActivePage] = useState(1);
+	const [itemsPerPage, setItemsPerPage] = useState(5);
+	const [pageRange, setPageRange] = useState(5);
 
-	componentDidMount = () => {
-		axios
-			.get(`${domain}/recipes.php`)
-			.then((response) => {
-				console.log(response.data);
-				this.setState({
-					recipes: response.data,
-				});
-				this.setState({
-					isLoading: false,
-				});
-			})
-			.catch(function (error) {
-				console.log(error);
-			});
-	};
+	useEffect(() => {
+		const getRecipes = async () => {
+			const results = await axios.get(`${domain}/recipes.php`);
+			setRecipes(results.data);
+			setIsLoading(false);
+		};
+		getRecipes();
+	}, []);
 
-	// getCategoryName = () => {
-	// 	const { recipes } = this.state;
-	// 	return recipes.c_name;
-	// };
-
-	recipeList(renderedRecipes) {
+	const recipeList = (renderedRecipes) => {
 		return renderedRecipes.map((recipe) => (
 			<div className='recipe-link' key={recipe.id}>
-				<Link
-					to={'/recipes/' + recipe.id}
-					// className='nav-link btn btn-outline-info btn-sm'
-					// type='button'
-				>
-					{recipe.name}
-				</Link>
+				<Link to={'/recipes/' + recipe.id}>{recipe.name}</Link>
 			</div>
 		));
-	}
-
-	handlePageChange = (pageNumber) => {
-		console.log(`active page is ${pageNumber}`);
-		this.setState({ activePage: Number(pageNumber) });
 	};
 
-	render() {
-		const { isLoading } = this.state;
-		const indexOfLastRecipe = this.state.activePage * this.state.itemsPerPage;
-		const indexOfFirstRecipe = indexOfLastRecipe - this.state.itemsPerPage;
-		const renderedRecipes = this.state.recipes.slice(
-			indexOfFirstRecipe,
-			indexOfLastRecipe
-		);
-		console.log(renderedRecipes);
-		return (
-			<>
-				<h5 align='center'>Recipes</h5>
+	const handlePageChange = (pageNumber) => {
+		console.log(`active page is ${pageNumber}`);
+		setActivePage(Number(pageNumber));
+	};
 
-				{isLoading ? (
-					<div>
-						<LoadingSpinner />
+	const indexOfLastRecipe = activePage * itemsPerPage;
+	const indexOfFirstRecipe = indexOfLastRecipe - itemsPerPage;
+	const renderedRecipes = recipes.slice(indexOfFirstRecipe, indexOfLastRecipe);
+	console.log(renderedRecipes);
+	return (
+		<>
+			<h5 align='center'>Recipes</h5>
+
+			{isLoading ? (
+				<div>
+					<LoadingSpinner />
+				</div>
+			) : (
+				<>
+					<div className='recipes-container'>{recipeList(renderedRecipes)}</div>
+					<div className='pagination-container'>
+						<Paginate
+							activePage={activePage}
+							itemsCountPerPage={itemsPerPage}
+							totalItemsCount={recipes.length}
+							pageRangeDisplayed={pageRange}
+							handlePageChange={handlePageChange}
+							prevPageText='<'
+							nextPageText='>'
+							firstPageText='<<'
+							lastPageText='>>'
+						></Paginate>
 					</div>
-				) : (
-					<>
-						{/* <Link
-							to={'/add/' + this.props.match.params.id}
-							className='nav-link btn btn-outline-info btn-sm'
-							type='button'
-						>
-							Add
-						</Link> */}
-						<div className='recipes-container'>
-							{this.recipeList(renderedRecipes)}
-						</div>
-						<div className='pagination-container'>
-							<Paginate
-								activePage={this.state.activePage}
-								itemsCountPerPage={this.state.itemsPerPage}
-								totalItemsCount={this.state.recipes.length}
-								pageRangeDisplayed={this.state.pageRage}
-								handlePageChange={this.handlePageChange}
-								prevPageText='<'
-								nextPageText='>'
-								firstPageText='<<'
-								lastPageText='>>'
-							></Paginate>
-						</div>
-					</>
-				)}
-			</>
-		);
-	}
-}
+				</>
+			)}
+		</>
+	);
+};
+
+export default Recipes;
