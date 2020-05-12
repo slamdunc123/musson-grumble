aeca90361384c4322df2404a75566fd470af1646
duncanlaidlaw1 <slamdunc123@gmail.com>
2020-05-12 22:15:03 +0100

    add useHistory to always go to home after splashscreen enter clicked

--- src/components/pages/SplashScreen/SplashScreen.js
+++ src/components/pages/SplashScreen/SplashScreen.js
@@ -1,13 +1,16 @@
 import React, { useContext } from 'react';
 import { BrandContext } from '../../../context/context';
+import { useHistory } from 'react-router-dom';
 
 // css
 import './splashscreen.scss';
 
 const SplashScreen = ({ callback }) => {
+	let history = useHistory();
 	const brand = useContext(BrandContext);
 	// receive props from App.js (parent) in this case the callBackSplash method
 	const handleOnClick = () => {
+		history.push('/');
 		callback(); // pass value 'false' up to App.js (parent) through the callBackSplash method
 	};
 
