659e43de935ac4bd0ef28e1a429023e0ccc5a52a
duncanlaidlaw1 <slamdunc123@gmail.com>
2020-05-24 19:36:52 +0100

    change categories responsive settings and image presentation

--- src/components/pages/Recipes/Category.js
+++ src/components/pages/Recipes/Category.js
@@ -8,47 +8,18 @@ const Category = ({ category }) => {
 		return categoryName.toLowerCase();
 	};
 	return (
-		<>
-			<div className={`category-block small ${changeText()}`}>
-				<div className='block-header'>
-					<h6>
+		<div className={`category-block`}>
+			<Link to={'/categories/' + category.id}>
+				<div className='category-list-item'>
+					<div
+						className={`category-list-item-image ${changeText()}`}
+					></div>
+					<div className='category-list-item-name'>
 						{category.name}
-						<span> - {category.sub_title}</span>
-					</h6>
+					</div>
 				</div>
-
-				<div className='block-body'>
-					{/* <p className='block-text'>{category.description}</p> */}
-
-					<Link
-						to={'/categories/' + category.id}
-						className='btn btn-outline-light btn-sm'
-					>
-						View
-					</Link>
-				</div>
-			</div>
-
-			<div className={`category-block large ${changeText()}`}>
-				<div className='block-header'>
-					<h6>
-						{category.name}
-						<span> - {category.sub_title}</span>
-					</h6>
-				</div>
-				<div className='block-body'>
-					{/* <p className='block-text'>{category.description}</p> */}
-
-					<Link
-						to={'/categories/' + category.id}
-						className='btn btn-outline-light btn-sm'
-					>
-						View
-					</Link>
-				</div>
-			</div>
-			{/* )} */}
-		</>
+			</Link>
+		</div>
 	);
 };
 
