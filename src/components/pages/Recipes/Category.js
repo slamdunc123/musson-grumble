a4d75ec4e652487d4d1bcba8eebaaa8fa38ec8ff
duncanlaidlaw1 <slamdunc123@gmail.com>
2020-05-24 10:31:54 +0100

    move category styles categories.scss

--- src/components/pages/Recipes/Category.js
+++ src/components/pages/Recipes/Category.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './recipes.scss';
+import './categories.scss';
 
 const Category = ({ category }) => {
 	const changeText = () => {
